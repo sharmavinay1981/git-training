63371c4b0911a4d939cde073b8f0bd82dc71badf
UdaySainath <mouli333.g@gmail.com>
2024-04-25 15:21:58 +0530

    my final changes

---
+++ .history/sample_20240425151554.js
@@ -0,0 +1,19 @@
+// Vamsee
+function add(a, b) {
+  return 0;
+}
+
+// Mauli
+<<<<<<< Updated upstream
+function add2(a, b) {
+  return 0;
+=======
+function add2(a,b){
+    return a+b;
+>>>>>>> Stashed changes
+}
+
+// Pradyumn
+function add3(a, b) {
+  return a + b;
+}
---
+++ .history/sample_20240425151617.js
@@ -0,0 +1,14 @@
+// Vamsee
+function add(a, b) {
+  return 0;
+}
+
+// Mauli
+function add2(a,b){
+    return a+b;
+}
+
+// Pradyumn
+function add3(a, b) {
+  return a + b;
+}
