7874b62c10a3582969316fdeb448cb58d8e27519
Vinay Kumar Sharma <vinay.sharma1201@gmail.com>
2024-04-30 14:30:08 +0530

    Updated the contact information on the page.

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -4,6 +4,8 @@ export default function Contact() {
   return (
     <div>
       <h1>Contact Us</h1>
+      <h4>H Block, Sector 63, Noida, Uttar Pradesh, India</h4>
+      <p>We are happy to help you on any query. Please feel free to share your questions at <b>queries@dummy.com</b></p>
     </div>
   );
 }
